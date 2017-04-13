32b0d07c44dc6612aee370037a2826ba4bf83054
Barizon <rafael.barizon@cwi.com.br>
2017-04-13 16:12:39 -0300

    Salvando as cosias

---
+++ BlueJ1stProjectSaintSeiya/Armor.java
@@ -0,0 +1,8 @@
+public class Armor {
+    private String armor;
+    
+    public Armor(String armor){
+        this.armor = armor;
+   }
+    
+}    
--- BlueJ1stProjectSaintSeiya/Saint.java
+++ BlueJ1stProjectSaintSeiya/Saint.java
@@ -1,33 +1,14 @@
-
-/**
- * Write a description of class Saint here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Saint
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Saint
-     */
-    public Saint()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+public class Saint {
+    private String name = "Seiya";
+    private Armor armor;
+    private boolean suited;
+    
+    public Saint(String name, Armor armor) {
+        this.name = name;
+        this.armor = armor;
+    }   
+    
+    public void suitUpArmor(){
+        this.suited = true;
+    }    
 }
