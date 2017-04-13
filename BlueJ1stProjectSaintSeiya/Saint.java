f8b58d80683da4bf526e855861d3bdb8663c96db
Barizon <rafael.barizon@cwi.com.br>
2017-04-13 16:21:38 -0300

    Commit with ignore

--- BlueJ1stProjectSaintSeiya/Saint.java
+++ BlueJ1stProjectSaintSeiya/Saint.java
@@ -2,7 +2,7 @@ public class Saint {
     private String name = "Seiya";
     private Armor armor;
     private boolean suited;
-    
+    /* */
     public Saint(String name, Armor armor) {
         this.name = name;
         this.armor = armor;
