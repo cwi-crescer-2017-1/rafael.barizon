374a45e4d7706212b5b9b63fb57fd3f59780cce5
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-04-17 08:56:30 -0300

    feat (fight)
    Novas implementacoes no metodo Fight, fazendo com que quando comeca a luta ela termina quando um dos lutadores tenha morrido

--- BlueJ1stProjectSaintSeiya/Battle.java
+++ BlueJ1stProjectSaintSeiya/Battle.java
@@ -12,6 +12,7 @@ public class Battle
     private Saint saintTwo;
     private Armor armorSaintOne;
     private Armor armorSaintTwo;
+    private int order;
     
     public Battle(Saint saintOne, Saint saintTwo){
         this.saintOne = saintOne;
@@ -22,9 +23,46 @@ public class Battle
         this.armorSaintOne = saintOne.getArmor();
         this.armorSaintTwo = saintTwo.getArmor();
         
+        System.out.println(saintOne.getName() + "(" + saintOne.getLife()+")"+ " vs. " + saintTwo.getName()+ "(" + saintTwo.getLife()+")" + "\n");
         
-        if(armorSaintOne.getCategory().getValor() >= armorSaintTwo.getCategory().getValor()){
-            saintTwo.loseLifePoints(damage);
-        }else saintOne.loseLifePoints(damage);
+        
+        //while(saintOne.getLife() > 0.0 && saintTwo.getLife() > 0.0){
+            if(armorSaintOne.getCategory().getValor() >= armorSaintTwo.getCategory().getValor()){
+                saintTwo.loseLifePoints(damage);
+                order = 1;
+                System.out.println(saintOne.getName() + " Attack and " + saintTwo.getName() + " now has " + saintTwo.getLife() + " life Points\n");
+            }else {
+                saintOne.loseLifePoints(damage);
+                order = 2;
+                System.out.println(saintTwo.getName() + " Attack and " + saintOne.getName() + " now has " + saintOne.getLife() + " life Points\n");
+            }
+            
+            if(order == 1){
+                while (saintOne.getLife() > 0.0 && saintTwo.getLife() > 0.0){
+                    saintOne.loseLifePoints(damage);
+                    System.out.println(saintTwo.getName() + " Attack and " + saintOne.getName() + " now has " + saintOne.getLife() + " life Points\n");
+                    saintTwo.loseLifePoints(damage);
+                    System.out.println(saintOne.getName() + " Attack and " + saintTwo.getName() + " now has " + saintTwo.getLife() + " life Points\n");
+                }
+                
+            } else if(order == 2){
+                 while (saintOne.getLife() > 0.0 && saintTwo.getLife() > 0.0){
+                    saintTwo.loseLifePoints(damage);
+                    System.out.println(saintOne.getName() + " Attack and " + saintTwo.getName() + " now has " + saintTwo.getLife() + " life Points\n");
+                    saintOne.loseLifePoints(damage);
+                    System.out.println(saintTwo.getName() + " Attack and " + saintOne.getName() + " now has " + saintOne.getLife() + " life Points\n");
+                }
+            }
+            
+            
+            
+            if (saintOne.getLife() == 0.0) {
+                saintOne.setStatus(Status.DEAD);
+                System.out.println("Saint " + saintOne.getName() + " died\n");
+            }else{     
+                saintTwo.setStatus(Status.DEAD);
+                System.out.println("Saint " + saintTwo.getName() + " died\n");
+            }
+        //}
     }
 }
