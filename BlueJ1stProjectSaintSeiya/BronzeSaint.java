6010b758a5cd4973459b1a013969b44d84f96b0f
Barizon <rafael.barizon@cwi.com.br>
2017-04-17 17:02:49 -0300

    feat(BronzeSaint): criado filho de Saint adicionado heranca

---
+++ BlueJ1stProjectSaintSeiya/BronzeSaint.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class BronzeSaint here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BronzeSaint extends Saint{
+    
+    
+    public BronzeSaint (String nome, Armadura armadura) throws Exception {
+        super(nome,armadura);
+        this.qtsSentidosDespertados = 5;
+    }
+    
+}
