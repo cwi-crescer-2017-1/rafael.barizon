b0bfc8cb9b066785de50d1df96cd490268162be5
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-04-18 10:23:17 -0300

    feat(Constelacao):criado classe constelacao

---
+++ BlueJ1stProjectSaintSeiya/Constelacao.java
@@ -0,0 +1,22 @@
+public class Constelacao
+{
+    private String nome;
+    private Golpe[] golpes = new Golpe[3];
+    private int ultimoGolpe = 0;
+    
+    public Constelacao(String nome){
+        this.nome = nome;
+    }
+    
+    public void adicionarGolpe(Golpe golpe) {
+        if(ultimoGolpe<10){
+            this.golpes[ultimoGolpe] = golpe;
+            this.ultimoGolpe++;
+        }else System.out.println("Numero maximo de golpes atingido\n");
+    }
+    
+    public Golpe[] getGolpes(){
+        return this.golpes;
+    }
+    
+}
