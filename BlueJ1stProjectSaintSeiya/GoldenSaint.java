29d6785d644c9e618ac88f7b5d8e181507939910
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-04-18 09:19:53 -0300

    refactor(goldSaint): arrumano nome da classe deletando classe do master

--- BlueJ1stProjectSaintSeiya/GoldenSaint.java
+++
@@ -1,8 +0,0 @@
-public class GoldenSaint extends Saint
-{
-
-    public GoldenSaint(String nome, Armadura armadura) throws Exception {
-        super(nome,armadura);
-        this.qtsSentidosDespertados = 7;
-    }
-}
