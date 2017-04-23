f19137d4754506859734733fb98c90dbe491961b
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-04-23 19:14:11 -0300

    feat(Golpear): criado implements golpear

---
+++ BlueJ1stProjectSaintSeiya/Golpear.java
@@ -0,0 +1,19 @@
+public class Golpear implements Movimento{
+        Saint golpeador;
+    Saint golpeado;
+    public Golpear(Saint golpeador, Saint golpeado) {
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar() throws Exception{
+        double multiplicadorDano = 1.0;
+        double dano;
+        if(golpeador.getArmaduraVestida()){
+            multiplicadorDano *= golpeador.getArmadura().getCategoria().getValor();
+        }
+        Golpe golpe = golpeador.getProximoGolpe();
+        dano = golpe.getFatorDano() * multiplicadorDano ;
+        golpeado.perdeVida(dano);
+    }
+}
