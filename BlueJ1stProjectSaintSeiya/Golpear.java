07fb0d87d364419fdc78750ec6f2cd7a15c78da1
Barizon <rafael.barizon@cwi.com.br>
2017-04-24 17:39:29 -0300

    refactor(Golpear): arrumado algumas linhas

--- BlueJ1stProjectSaintSeiya/Golpear.java
+++ BlueJ1stProjectSaintSeiya/Golpear.java
@@ -1,6 +1,7 @@
 public class Golpear implements Movimento{
-        Saint golpeador;
+    Saint golpeador;
     Saint golpeado;
+    
     public Golpear(Saint golpeador, Saint golpeado) {
         this.golpeador = golpeador;
         this.golpeado = golpeado;
@@ -10,7 +11,7 @@ public class Golpear implements Movimento{
         double multiplicadorDano = 1.0;
         double dano;
         if(golpeador.getArmaduraVestida()){
-            multiplicadorDano *= golpeador.getArmadura().getCategoria().getValor();
+            multiplicadorDano += golpeador.getArmadura().getCategoria().getValor();
         }
         Golpe golpe = golpeador.getProximoGolpe();
         dano = golpe.getFatorDano() * multiplicadorDano ;
