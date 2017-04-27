6ed6a9b5885162fc8702318c74e75d9ff992fbf6
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-04-27 11:50:16 -0300

    refactor(AtaqueDuplo):trocado nome de variavel

--- BlueJ1stProjectSaintSeiya/AtaqueDuplo.java
+++ BlueJ1stProjectSaintSeiya/AtaqueDuplo.java
@@ -3,14 +3,6 @@ import java.util.Random;
 public class AtaqueDuplo implements Movimento{
     private Saint golpeador, golpeado;
     
-    
-    
-    /**
-     Roll The Dice
-O golpeador tem 33.3% de chance de desferir um ataque com o dobro do dano (que deve seguir as regras normais de dano, já implementadas previamente).
-Caso o golpeador não consiga desferir o ataque duplo (ou seja, estiver fora dos 33.3%), ele deve desferir um ataque normalmente mas perde 5% de vida. 
-Quando o Saint tiver menos que 1 de vida ele morre (exemplo: 0.99 de vida já é considerado morto).
-     */
     public AtaqueDuplo(Saint golpeador, Saint golpeado){
         this.golpeador = golpeador;
         this.golpeado = golpeado;
@@ -23,9 +15,8 @@ Quando o Saint tiver menos que 1 de vida ele morre (exemplo: 0.99 de vida já é
     public boolean sortear(){
         final Random random = new Random();
         final int min =1, max = 3;
-        if (random.nextInt(max-min+1)+min == 1)
-            return true;
-        return false;
+        boolean vaiAtacarComDanoDuplo = random.nextInt(max-min+1)+min == 1;
+        return vaiAtacarComDanoDuplo;
     }
     
     public void ataqueDuplo() throws Exception{
