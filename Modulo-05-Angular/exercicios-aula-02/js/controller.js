8a5180cd37c08e19f16dc58480ef848be0b05e9d
Barizon <rafael.barizon@cwi.com.br>
2017-05-18 17:11:28 -0300

    feat(mascada filter): criado filter para retornar $ Nunes $

--- Modulo-05-Angular/exercicios-aula-02/js/controller.js
+++ Modulo-05-Angular/exercicios-aula-02/js/controller.js
@@ -1,27 +1,42 @@
 var myapp =  angular.module('myApp',[])
 
 myapp.controller('controller',['$scope', function($scope){
-
+  var instrutores = [{
+      nome: 'Bernardo',
+      aula: [{
+         numero: 1,
+         nome: 'OO'},
+        {numero: 4,
+         nome: 'Javascript'}]},
+    {
+      nome: 'Nunes',
+      aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'}]},
+    {
+      nome: 'Pedro (PHP)',
+      aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'}]},
+    {
+      nome: 'Zanatta',
+      aula: [{
+        numero: 5,
+        nome: 'AngularJS'}]}
+  ];
+  $scope.instrutores = instrutores;
   $scope.minhaFuncao = function(){
-
     var data = $scope.dataDigitada;
     data = data.split('/');
-    console.log(data);
     data = new Date(data[2], data[1]-1, data[0])
     $scope.dataTerminada = data;
-    console.log(data);
-
-    console.log("Clicou");
   }
-  // 
-  //
-  // $scope.outraFuncao = function(){
-  //
-  //
-  //   let dataFormatada = $scope.dataDigitada.split('/');
-  //
-  //
-  //
-  // }
-  console.log("oi");
 }])
+
+myapp.filter('mascada', function(){
+  return function(nome){
+     return nome.replace(/(nunes)/i,'$ $1 $')
+    // return  nome.replace(/(nunes|bernardo|zanatta)/i,'$ $1 $')
+
+  }
+})
