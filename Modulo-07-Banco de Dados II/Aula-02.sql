ca5fb7970687fade59475426f1ba9a5cc60a9bdd
Barizon <rafael.barizon@cwi.com.br>
2017-06-14 15:26:37 -0300

    feat(exercicios 03-04)

--- Modulo-07-Banco de Dados II/Aula-02.sql
+++ Modulo-07-Banco de Dados II/Aula-02.sql
@@ -12,17 +12,17 @@ Faça um código (PL/SQL) que liste quais são as cidades duplicadas. Após isso
 liste todos os clientes que estão relacionados com estas cidades
 */
 --select * from cidade
-/*
+
 DECLARE
  CURSOR c_ListaCliente IS
     select cliente.nome 
       from cliente 
       join cidade on cliente.idcidade = cidade.idcidade 
-      where cidade.nome in (select nome from (
-                               Select Nome, count(nome) as quantidade, uf
+      where (cidade.nome, cidade.uf )in (
+                               Select Nome, uf
                                  From   Cidade
                                  group by nome, uf
-                                 having count(nome) > 1)
+                                 having count(nome) > 1
                                );
 BEGIN 
    for clienteLista in c_ListaCliente loop
@@ -30,7 +30,6 @@ BEGIN
    end loop;
 END;
 
-*/
 
 /*
 Exercício 2
@@ -54,7 +53,7 @@ declare
   vIdPedido pedido.idpedido%type;
 begin
   vValorTotal := 0.0;
-  vIdPedido := 15;
+  vIdPedido := 123;
 
   for item in C_ItensPedido(vIdPedido) loop
    vValorTotal := vValorTotal + (item.quantidade * item.precounitario);
@@ -64,11 +63,79 @@ begin
      where idPedido = vIdPedido;
 end;
 
+select * from pedido where idpedido = 123;
 
+/*Exercício 3
 
+Atualização de Clientes
 
+Crie uma rotina que atualize todos os clientes que não realizaram nenhum pedido 
+nos últimos 6 meses (considere apenas o mês, dia 01 do 6º mês anterior). 
+Definir o atributo Situacao para I.
+*/
+--update cliente situacao = 'I' where idcliente not in (C_ClientesAtivos6Meses)
+begin
+  update cliente 
+    set situacao = 'I' 
+    where idcliente not in (
+    select c.IDCLIENTE
+        from cliente c
+        join pedido p on p.idcliente = c.idcliente 
+        and p.datapedido >= add_months(trunc(sysdate), -6 )
+        group by c.idcliente
+        );
+end;
+/*
+Exercício 4
 
+Previsão de Materiais
+Faça uma rotina que receba dois parâmetros:
+IDProduto
+Mês e Ano
+E então faça uma rotina que verifique no ANO/MÊS para o produto informado
+qual a lista de materiais (incluindo a quantidade) é necessário para atender 
+todos os Pedidos desde período. Deve imprimir o nome do material e quantidade total.
+*/
 
+declare
+  cursor c_ListaQuantidadeDoPedido(ano in numeric, mes in numeric, idProd in numeric) is
+      select sum(pedItem.quantidade) as soma from pedido ped
+      join pedidoitem pedItem on pedItem.IDPEDIDO = ped.IDPEDIDO
+      and pedItem.IDPRODUTO = idProd
+      where extract(year from ped.datapedido) = ano and
+            extract(month from ped.datapedido) = mes;
+  cursor c_ListaMatQtdDesc(idProd in numeric) is
+      select mat.descricao, mat.PESOLIQUIDO, prodm.quantidade 
+      from produtomaterial prodM 
+      join material mat on mat.idmaterial = prodm.idmaterial
+      where prodM.idproduto = idProd;
+ 
+  vAno numeric;
+  vMes numeric;
+  vIDProduto numeric;
+  vQuantidadeTOtalPedido PEDIDOITEM.QUANTIDADE%type;
+  vPesoVesesQtd MATERIAL.PESOLIQUIDO%type;
+begin
+  vAno := 2017;
+  vMes := 04;
+  vIDProduto := 4246;
+  vQuantidadeTOtalPedido := 0.0;
+  vPesoVesesQtd := 0.0;
+  
+  for somaLista in c_ListaQuantidadeDoPedido(vAno, vMes, vIDProduto) loop
+    vQuantidadeTOtalPedido := vQuantidadeTOtalPedido + somaLista.soma;
+    DBMS_OUTPUT.PUT_LINE(somaLista.soma);
+  end loop;
+  
+  for materialLista in c_ListaMatQtdDesc(vIDProduto)loop 
+    --DBMS_OUTPUT.PUT_LINE(materialLista.PESOLIQUIDO || ' - ' || materialLista.quantidade || ' - ' || vQuantidadeTOtalPedido);
+    vPesoVesesQtd := vQuantidadeTOtalPedido * (  materialLista.PESOLIQUIDO * materialLista.quantidade );
+    if (vPesoVesesQtd > 0) then
+    DBMS_OUTPUT.PUT_LINE(materialLista.descricao || ' - ' || to_char(vPesoVesesQtd) );
+    end if;
+    --Deve imprimir o nome do material e quantidade total.
+  end loop;
+end;
 
 
 
