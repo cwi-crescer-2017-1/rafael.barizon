a68c82e7adede757e29839c832d16ea34f722aa6
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-06-15 23:32:07 -0300

    feat(mega) inicio do pensamento do trabalho

---
+++ Modulo-07-Banco de Dados II/MEGASENADAT.sql
@@ -0,0 +1,30 @@
+select * from aposta;
+select * from aposta_premiada;
+select * from cidade;
+select * from concurso;
+select * from numero_aposta;
+select * from REGRA_RATEIO_PREMIO;
+
+
+
+
+
+
+-- function f_cadastrar_concurso (numConcurso, dataSorteio, ValorBrutoPremio)
+-- function f_countNumerosAcertados (idAposta) - usa o numero_aposta que contem os numeros apostados
+-- procedure insert_ganhador - aposta_premiada (idaposta_premiada, idaposta, acertos, valor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
