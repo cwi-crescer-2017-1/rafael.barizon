989c37627375f81f8c45b7f2152c339ef813b017
Rafael Barizon <rafael.barizon@cwi.com.br>
2017-06-21 00:56:01 -0300

    fix(pastas) arrumado pastas de arquivos da aula -01

--- Modulo-08-Java/aula1/src/main/java/br/com/crescer/aula1/ExercicioEnum.java
+++ Modulo-08-Java/aula1/src/main/java/br/com/crescer/aula1/ExercicioEnum.java
@@ -9,32 +9,32 @@ import java.util.Collections;
  */
 public class ExercicioEnum {
     public static void main(String[] args) {
-        String hi = "1234567890";
-        hi = new StringBuilder(hi).reverse().toString();
-        System.out.println(hi);
+//        String hi = "1234567890";
+//        hi = new StringBuilder(hi).reverse().toString();
+//        System.out.println(hi);
+//        
+//        String string = "HaHeù";
+//        string =
+//                Normalizer
+//                .normalize(string, Normalizer.Form.NFD)
+//                .replaceAll("[^\\p{ASCII}]", "");
+//        System.out.println(string);
+//        
+//        String palindromo = "A ,sogra má, e amargosa";
+//        palindromo = Normalizer
+//                .normalize(palindromo, Normalizer.Form.NFD)
+//                .replaceAll("[^\\p{ASCII}]", "")
+//                .toLowerCase()
+//                .replaceAll("[^a-zA-Z]", "");
+//        
+//       // String pal = palindromo;
+//        
+//        System.out.println(" Verificando se A sogra má e amargosa eh palindromo: " + palindromo + " string invertida : " + new StringBuilder(palindromo).reverse().toString());
+//        System.out.println(palindromo.equals(new StringBuilder(palindromo).reverse().toString()));
+//        
         
-        String string = "HaHeù";
-        string =
-                Normalizer
-                .normalize(string, Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "");
-        System.out.println(string);
-        
-        String palindromo = "A ,sogra má, e amargosa";
-        palindromo = Normalizer
-                .normalize(palindromo, Normalizer.Form.NFD)
-                .replaceAll("[^\\p{ASCII}]", "")
-                .toLowerCase()
-                .replaceAll("[^a-zA-Z]", "");
-        
-       // String pal = palindromo;
-        
-        System.out.println(" Verificando se A sogra má e amargosa eh palindromo: " + palindromo + " string invertida : " + new StringBuilder(palindromo).reverse().toString());
-        System.out.println(palindromo.equals(new StringBuilder(palindromo).reverse().toString()));
         
         
-        
-        /*
         ArrayList<String> estados = new ArrayList<String>();
         for (Estados n : Estados.values()) {
            estados.add(n.getNome());
@@ -46,6 +46,6 @@ public class ExercicioEnum {
         for ( int i = 1 ; i < estados.size(); i++){
             buffer.append(",\n" + estados.get(i));
         }
-        System.out.println(buffer);*/
+        System.out.println(buffer);
     }
 }
