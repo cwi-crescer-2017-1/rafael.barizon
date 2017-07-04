1322b85a3a100ac90b74bc645b3cfd8ac31f8a32
Barizon <rafael.barizon@cwi.com.br>
2017-07-04 11:43:27 -0300

    feat(social) last commit

--- Modulo-08-Java/social/src/front/js/controller.js
+++ Modulo-08-Java/social/src/front/js/controller.js
@@ -21,7 +21,6 @@ Social.controller('FeedController', function($scope, $routeParams, social, $loca
   }
 
   function getFrindsAndPending(){
-
     social.getRelations($rootScope.usuario.idUser).then(function (response){
         for (a of response.data){
           let friend = a;
@@ -37,24 +36,22 @@ Social.controller('FeedController', function($scope, $routeParams, social, $loca
     })
   }
 
-  console.log("ta dentro do feed controller");
-console.log("user ");
-  console.log($rootScope.usuario);
   getUsuarioperfil($rootScope.usuario.username);
 
   function gg(post){
     var liked = false;
-    var x;
+    var pular;
     for (p of post.contentLikes){
       let ind = p;
       if(p.idUser === $rootScope.usuario.idUser){
         liked = true;
-        x = post.contentLikes.indexOf(ind);
+        pular = post.contentLikes.indexOf(ind);
         break;
       }
     }
+
     if(liked){
-      post.contentLikes.splice(x,1);
+      post.contentLikes.splice(pular,1);
     }else{
       post.contentLikes.push($rootScope.usuario);
     }
@@ -99,13 +96,62 @@ console.log("user ");
 
 Social.controller('ProfileController', function($scope, $routeParams, social, $location, $rootScope){
   $scope.controller = 'ProfileController';
-  //$scope.postContent = postCont;
   $scope.userPosts = [{}];
   $scope.idUser = $routeParams.idUser;
   $scope.add = add;
   $scope.searchName = searchName;
   $scope.user;
   $scope.updateUser = updateUser;
+  $scope.profile0 = {};
+  $scope.profile0.friends=[];
+  $scope.goToProfile =goToProfile;
+  $scope.gg = gg;
+
+  function gg(post){
+    var liked = false;
+    var pular;
+    for (p of post.contentLikes){
+      let ind = p;
+      if(p.idUser === $rootScope.usuario.idUser){
+        liked = true;
+        pular = post.contentLikes.indexOf(ind);
+        break;
+      }
+    }
+
+    if(liked){
+      post.contentLikes.splice(pular,1);
+    }else{
+      post.contentLikes.push($rootScope.usuario);
+    }
+
+    social.postGG(post).then(function (response){
+      getPostsUser($rootScope.usuario.idUser);
+    })
+  }
+
+  function goToProfile(id){
+    $location.path('profile/'+id);
+  }
+  getFriends();
+
+  function getFriends(){
+    let id;
+    $scope.idUser? id = $scope.idUser: id = $rootScope.usuario.idUser;
+    social.getRelations(id).then(function (response){
+        for (a of response.data){
+          let friend = a;
+            social.getUserprofileById(friend.relationshipPK.idUserRelationship).then(function (response){
+              response.data.status = friend.relationshipStatus;
+              if(friend.relationshipStatus === 'accepted'){
+                $scope.profile0.friends.push(response.data);
+              }
+            })
+          }
+          console.log("termino o for pegando amigos");
+    })
+  }
+
   function searchName(nameSearch){
     console.log(nameSearch);
     social.getUsersByName(nameSearch).then(function (response){
@@ -144,6 +190,7 @@ Social.controller('ProfileController', function($scope, $routeParams, social, $l
       $rootScope.user.birthday = new Date($rootScope.usuario.birthday);
       console.log("helloo");
       console.log($scope.user);
+
     })
   }
 
@@ -189,7 +236,7 @@ Social.controller('RequestController', function($scope, $routeParams, social, $l
   $scope.friends=[];
   $scope.pending=[];
   $scope.changePassword = changePassword;
-  
+
 //  $scope.relationshipUpdate = relationshipUpdate;
   getUsuarioperfil($rootScope.usuario.username);
   getUserRelationship($rootScope.usuario.idUser);
